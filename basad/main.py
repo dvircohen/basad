da87f507b91d1787301d39ab12f93ac0b2feefb9
Dvir <dvir.cohn@gmail.com>
2019-12-18 15:48:45 +0200

    added the ShabbatChecker class

--- basad/main.py
+++ basad/main.py
@@ -1,5 +1,36 @@
+import requests
+import sys
+import json
+import datetime
 
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
 
-class BasadPrinter():
+class ShabbatCheker():
     def __init__(self):
-        print("Basad")
+        pass
+
+    def check(self):
+        """
+        Use hebcal RESP API to check if the code is running on Shabbat
+        :return:
+        """
+        req = requests.get('https://www.hebcal.com/shabbat/?cfg=json&geonameid=3448439&m=50')
+        j = json.loads(req.text)
+
+        for item in j['items']:
+            if item['category'] == 'candles':
+                candles = item['date']
+                candles = datetime.datetime.strptime(candles[:-6], '%Y-%m-%dT%H:%M:%S')
+
+            if item['category'] == 'havdalah':
+                havdalah = item['date']
+                havdalah = datetime.datetime.strptime(havdalah[:-6], '%Y-%m-%dT%H:%M:%S')
+
+        if time_in_range(candles, havdalah, datetime.datetime.now()):
+            print("Your code ran on Shabbat and therefor not kosher. Exiting.")
+            sys.exit(0)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'basad',         # How you named your package folder (MyLib)
   packages = ['basad'],   # Chose the same as "name"
-  version = '0.12',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Thid library write Basad at the beginning of your code',   # Give a short description about your library
   author = 'Dvir Cohen',                   # Type in your name
@@ -11,7 +11,7 @@ setup(
   download_url = 'https://github.com/dvircohen/basad/archive/v_012.tar.gz',    # I explain this later on
   keywords = ['judaism', 'basad', 'BH'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-      ],
+      'requests '],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
